a5fa29df1f9585ba5f19e2508d304c36288d2b17
DorotaBdrn <dorotabiedron@icloud.com>
2020-03-30 16:17:11 +0200

    Start

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,4 +1,37 @@
 package com.company;
 
 public class Book {
+    private String title;
+    private String condition;
+    private String status;
+
+    public Book(String title, String condition, String status) {
+        this.title = title;
+        this.condition = condition;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
